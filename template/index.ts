3a0dd210bb961164d43fec23a9e8290806c7d62f
krishnau2 <krishnau2@gmail.com>
2025-01-03 23:27:38 +0100

    Added templates of files need to be copied to user's project

---
+++ template/index.ts
@@ -0,0 +1,10 @@
+console.log("This is a test")
+
+let message: string = "I am learning"
+message += " TypeScript"
+console.log("Message: ", message)
+
+// Uncomment the below lines to see the Type check errors in TypeScript
+
+// message = true;
+// message = 20;
